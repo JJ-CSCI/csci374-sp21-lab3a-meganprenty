02c05f388bd5491c5c7b941965538e04ad43d231
meganprenty <meganprenty94@gmail.com>
2021-03-03 22:56:09 +0000

    metrics

--- main.fs
+++ main.fs
@@ -3,37 +3,37 @@ module Assignment
 // DO NOT MODIFY LEFT PART OF `LET` OPERATOR
 
 // Define following distance unit transformations
-let inchToMillimeter v =
-    0.0 // write your code here
+let inchToMillimeter v = v * 25.4;;
+     // write your code here
 
-let footToInch x =
-    0.0 // write your code here
+let footToInch x = x * 12;;
+     // write your code here
 
-let yardToFoot v =
-    0.0 // write your code here
+let yardToFoot v = v * 3;;
+     // write your code here
 
-let mileToYard mile =
-    0.0 // write your code here
+let mileToYard mile = mile * 1760;;
+    // write your code here
 
 (*
 Use ONLY above functions and the forward pipe operator, `|>`,  to define the following unit transformation
 
 Forward pipe operator passes the result of the left side to the function on the right side.
 *)
-let yardToMillimeter y =
-    0.0 // write your code here
+let yardToMillimeter y = y |> yardToFoot |> footToInch |> inchToMillimeter;;
+     // write your code here
 
 // Define the curried function `divBy1000` that has the signature `float -> float -> float`
 let divBy1000 =
-    fun x->x // write your code here
+    fun x->x/ 1000.0 // write your code here
 
 (*
 Use ONLY above functions to define the following two functions. DO NOT CHANGE SIGNATURES!
 *)
-let millimeterToMeter n =
-    0.0 // write your code here
+let millimeterToMeter n = n |> divBy1000;;
+     // write your code here
 
-let meterToKilometer =
+let meterToKilometer m = m * 1000;;
     fun x->x // write your code here
 
 (*
@@ -41,13 +41,13 @@ Use above functions and function composition operator, `>>`, to define the follo
 
 Forward composition operator composes two functions into one.
 *)
-let millimeterToKilometer =
-    fun x->x // write your code here
+let millimeterToKilometer x=
+    fun x->x/ 3000;;  // write your code here
 
-let inchToMeter =
-    fun x->x // write your code here
+let inchToMeter x =
+    fun x->x * 25.4;; // write your code here
 
-let yardToKilometer =
+let yardToKilometer x=
     fun x->x // write your code here
 
 let mileToMeter =
@@ -56,13 +56,13 @@ let mileToMeter =
 
 // Define following weight unit transformations
 let ounceToGram v =
-    0.0 // write your code here
+    v * 28.35;; // write your code here
 
 let poundToOunce p =
-    0.0 // write your code here
+    p * 16;; // write your code here
 
 let shortTonToPound t =
-    0.0 // write your code here
+    t * 2000;; // write your code here
 
 
 (*
@@ -70,7 +70,7 @@ Use ONLY above functions to define the following two functions. DO NOT CHANGE SI
 *)
 
 let gramToKilogram g =
-    0.0 // write your code here
+    g * 1000;; // write your code here
 
 (*
 Use ONLY above functions and function composition operator, `>>`, to define the following unit transformations
